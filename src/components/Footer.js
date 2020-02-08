2308000af2e8385194c61c88f0d542d9e06320b1
Mischa Colley <me@mischacolley.com.au>
2020-02-08 14:09:29 +1100

    link updates

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,11 +8,7 @@ class Footer extends React.Component {
                     <ul className="icons">
                         <li><a href="tel:+61401969190" className="icon fa-phone"><span className="label-phone">+61401969190</span></a></li>
                         <li><a href="#contact" className="icon fa-envelope-o"><span className="label">Email</span></a></li>
-                        <li><a href="https://www.instagram.com/doula.sandra/" className="icon fa-instagram"><span className="label">Instagram</span></a></li>
                     </ul>
-                    {/* <ul className="copyright">
-                        <li>&copy; Gatsby Starter Strata</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
-                    </ul> */}
                 </div>
             </div>
         )
--- src/pages/index.js
+++ src/pages/index.js
@@ -37,83 +37,89 @@ class Homepage extends React.Component {
             </ul>
           </section>
 
-            <section >
-              <h2>Latest Posts</h2>
+          <section >
+            <h2>Latest Posts</h2>
 
-              {posts.map(({ node }) => {
-                const title = get(node, 'frontmatter.title') || node.frontmatter.path
+            {posts.map(({ node }) => {
+              const title = get(node, 'frontmatter.title') || node.frontmatter.path
+              return (
+                <div key={node.frontmatter.path}>
+                  <h3>
+                    <Link to={node.frontmatter.path}>{title}</Link>
+                  </h3>
+                  <small>{node.frontmatter.date}</small>
+                  <p dangerouslySetInnerHTML={{ __html: node.excerpt }} />
+                </div>
+              )
+            })}
+
+            <ul className="actions">
+              <li>
+                <Link className="button" to="/posts">See More</Link>
+              </li>
+            </ul>
+              
+          </section>
+
+          <section id="two">
+            <h2>Latest Images</h2>
+
+            <ul className="latest-posts">
+              {instagram.map(({ node }) => {
                 return (
-                  <div key={node.frontmatter.path}>
-                    <h3>
-                      <Link to={node.frontmatter.path}>{title}</Link>
-                    </h3>
-                    <small>{node.frontmatter.date}</small>
-                    <p dangerouslySetInnerHTML={{ __html: node.excerpt }} />
-                  </div>
+                  <li>
+                    <a href={"https://www.instagram.com/p/" + node.id} target="_blank">
+                      <Img fluid={node.localFile.childImageSharp.fluid} />
+                    </a>
+                  </li>
                 )
               })}
+            </ul>
 
-              <ul className="actions">
-                <li>
-                  <Link className="button" to="/posts">View all</Link>
-                </li>
-              </ul>
-                
-            </section>
+            <ul className="actions">
+              <li>
+                <a href="https://www.instagram.com/doula.sandra/" className="icon fa-instagram button" target="_blank"><span className="label">Instagram</span> See More</a>
+              </li>
+            </ul>
 
-            <section id="two">
-              <h2>Latest Images</h2>
+          </section>
 
-              <ul className="latest-posts">
-                {instagram.map(({ node }) => {
-                  return (
-                    <li>
-                      <a href={"https://www.instagram.com/p/" + node.id} target="_blank">
-                        <Img fluid={node.localFile.childImageSharp.fluid} />
-                      </a>
-                    </li>
-                  )
-                })}
-              </ul>
-
-            </section>
-
-            <section id="three">
-              <h2 id="contact">Get In Touch</h2>
-              <p>Accumsan pellentesque commodo blandit enim arcu non at amet id arcu magna. Accumsan orci faucibus id eu lorem semper nunc nisi lorem vulputate lorem neque lorem ipsum dolor.</p>
-              <div className="row">
-                <div className="8u 12u$(small)">
-                  <form method="post" action="#">
-                    <div className="row uniform 50%">
-                      <div className="6u 12u$(xsmall)"><input type="text" name="name" id="name" placeholder="Name" /></div>
-                      <div className="6u 12u$(xsmall)"><input type="email" name="email" id="email" placeholder="Email" /></div>
-                      <div className="12u"><textarea name="message" id="message" placeholder="Message" rows="4"></textarea></div>
-                    </div>
-                  </form>
-                  <ul className="actions">
-                    <li><input type="submit" value="Send Message" /></li>
-                  </ul>
-                </div>
-              <div className="4u 12u$(small)">
-                <ul className="labeled-icons">
-                  <li>
-                    <h3 className="icon fa-home"><span className="label">Address</span></h3>
-                    1234 Somewhere Rd.<br />
-                    Nashville, TN 00000<br />
-                    United States
-                  </li>
-                  <li>
-                    <h3 className="icon fa-mobile"><span className="label">Phone</span></h3>
-                    +61401969190
-                  </li>
-                  <li>
-                    <h3 className="icon fa-envelope-o"><span className="label">Email</span></h3>
-                    <a href="#">me@sandracolley.com.au</a>
-                  </li>
+          <section id="three">
+            <h2 id="contact">Get In Touch</h2>
+            <p>Accumsan pellentesque commodo blandit enim arcu non at amet id arcu magna. Accumsan orci faucibus id eu lorem semper nunc nisi lorem vulputate lorem neque lorem ipsum dolor.</p>
+            <div className="row">
+              <div className="8u 12u$(small)">
+                <form method="post" action="#">
+                  <div className="row uniform 50%">
+                    <div className="6u 12u$(xsmall)"><input type="text" name="name" id="name" placeholder="Name" /></div>
+                    <div className="6u 12u$(xsmall)"><input type="email" name="email" id="email" placeholder="Email" /></div>
+                    <div className="12u"><textarea name="message" id="message" placeholder="Message" rows="4"></textarea></div>
+                  </div>
+                </form>
+                <ul className="actions">
+                  <li><input type="submit" value="Send Message" /></li>
                 </ul>
               </div>
+            <div className="4u 12u$(small)">
+              <ul className="labeled-icons">
+                <li>
+                  <h3 className="icon fa-home"><span className="label">Address</span></h3>
+                  Ocean Shores,
+                  NSW, 2483,
+                  Australia
+                </li>
+                <li>
+                  <h3 className="icon fa-mobile"><span className="label">Phone</span></h3>
+                    <a href="tel:+61401969190">+61 401 969 190</a>
+                </li>
+                <li>
+                  <h3 className="icon fa-envelope-o"><span className="label">Email</span></h3>
+                    <a href="mailto:me@sandracolley.com.au">me@sandracolley.com.au</a>
+                </li>
+              </ul>
             </div>
-          </section>
+          </div>
+        </section>
 
         </div>
 
