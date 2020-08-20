9d4bfc35fc5db08630e64435dc4e5a21e7f48cea
Mischa Colley <me@mischacolley.com.au>
2020-08-20 20:56:24 +1000

    doula copy update

--- src/pages/doula.js
+++ src/pages/doula.js
@@ -12,8 +12,8 @@ import doula_deck from '../assets/images/doula_deck.jpg'
 class Doula extends React.Component {
   render() {
 
-    const pageTitle = "Doula Sandra - Birthworker for your pregnancy, birth & postpartum journey"
-    const pageDescription = "As a doula I am your personal, non medical birth support servicing all areas in the Byron shire as well as Lennox Head, Lismore, Murwillumbah, Ballina, Tweed Heads & the Gold Coast."
+    const pageTitle = "Doula.Sandra - Birthworker for your pregnancy, birth & postpartum journey"
+    const pageDescription = "As a birth doula I am your personal, non medical birth support servicing all areas in the Byron shire as well as Lennox Head, Lismore, Murwillumbah, Ballina, Tweed Heads & the Gold Coast."
 
     const testimonials = get(this, 'props.data.allMarkdownRemark.edges')
 
@@ -33,7 +33,7 @@ class Doula extends React.Component {
 
             <h1>Doula.Sandra</h1>
 
-            <p className="lead">I am a loving, emapathic and passionate doula.</p>
+            <p className="lead">I am a loving, emapathic and passionate birth doula.</p>
 
             <p>I have mastered the art of following my intuition to put my knowledge into practice so you can get the best possible support in your birth journey.</p> 
 
@@ -41,31 +41,21 @@ class Doula extends React.Component {
 
             <p>You, your family and your birth experience are unique and it’s my practice to support your individual choices in a loving and respectful way. It is vital that you feel safe, loved and nurtured as you embark on the journey to birth your baby/s. I am here to support you and advocate for your needs with all my capacities.</p>
 
-            <p>I live with my beautiful family near Byron Bay in the Northern Rivers region of NSW, Australia. I service all areas in the Byron shire as well as Lennox Head, Lismore, Muhrwullumbah, Ballina, Tweed Heads and the Gold Coast.</p>
+            <p>I live with my beautiful family near Byron Bay in the Northern Rivers region of NSW, Australia. Bundjalung and Yugambeh country. I service all areas in the Byron shire as well as Lennox Head, Lismore, Muhrwullumbah, Ballina, Tweed Heads and the Gold Coast.</p>
 
             <img src={doula_deck} className="image" alt="Labouring on Deck" />
   
           </section>
 
-          {/* <section>
-
-            <h2>What i do?</h2>
-
-            <p>As a doula I am your personal, non medical birth support. Birth is a wonderfully and intense journey and if you can you should make use of as much support as possible. It can become vital to have a known, trusted, experienced person with you. I offer guidance and unbiased information on all topics around birth, pregnancy and the prenatal period.</p>
-
-            <p>I am experienced in accupressure, breathing/birthing techniques and other pain relief methods to ease the birth of your baby/ies. I can also provide you with nourishing home cooked meals, unlimited phone and messaging/email support and access to my resource library. If this sounds like the kind of birth support you're after, please contact me to schedule a complimentary first interview.</p>
-
-          </section> */}
-
           <section id="services">
 
             <h2>What I offer</h2>
 
-            <p>As a doula I am your personal, non medical birth support.</p>
+            <p>As a birth doula I am your personal, non medical birth support.</p>
 
             <p>I offer individualised educational, physical and emotional support during your pregnancy, birth and postnatal period.</p>
 
-            <p>Some days this might be a chat and a laugh or cry and other it might be practical or physical support. Some things you may choose to be part of your individual birth support are:</p>
+            <p>Some days this might be a chat and a laugh or cry and on another it might be practical or physical support. Some things you may choose to be part of your individual birth support include:</p>
 
             <ul>
               <li>Support of  your knowledge on how to safely birth your baby (i.e. personal birth prep. for you and your team)</li>
@@ -82,8 +72,6 @@ class Doula extends React.Component {
             </ul>
 
             <p>I'm prepared to stand up for you to support you in your choice and advocate for you. Ultimately support you in creating the birth you and your baby need for a bright future.</p>
-
-            <p></p>
 
           </section>
 
@@ -117,7 +105,7 @@ class Doula extends React.Component {
             <h2>I'd love to hear from you</h2>
             <div className="row">
               <div className="8u 12u$(small)">
-                <form method="post" action="#">
+                {/* <form method="post" action="#">
                   <div className="row uniform 50%">
                     <div className="6u 12u$(xsmall)"><input type="text" name="name" id="name" placeholder="Name" /></div>
                     <div className="6u 12u$(xsmall)"><input type="email" name="email" id="email" placeholder="Email" /></div>
@@ -126,16 +114,16 @@ class Doula extends React.Component {
                 </form>
                 <ul className="actions">
                   <li><input type="submit" value="Send Message" /></li>
-                </ul>
+                </ul> */}
               </div>
               <div className="4u 12u$(small)">
                 <ul className="labeled-icons">
-                  <li>
+                  {/* <li>
                     <h3 className="icon fa-home"><span className="label">Address</span></h3>
                     Ocean Shores,
                     NSW, 2483,
                     Australia
-                </li>
+                  </li> */}
                   <li>
                     <h3 className="icon fa-mobile"><span className="label">Phone</span></h3>
                     <a href="tel:+61401969190">+61 401 969 190</a>
